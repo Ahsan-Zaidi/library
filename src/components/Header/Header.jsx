160764d455b254d5d3be5dc672c9dab013a3cfb1
Zaidi <zaidiahsan.r@gmail.com>
2023-01-09 17:26:44 -0600

    Home/Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Navbar from '../Navbar/Navbar';
+import SearchForm from '../SearchForm/SearchForm';
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
-        
+    <div className='holder'>
+        <header className='header'>
+          <Navbar />
+          <div className='header-content flex flex-c text-center text-white'>
+            <h2 className='header-title text-capitalize'>
+              find your book of choice.
+            </h2><br />
+            <p className='header-text fs-18 fw-3'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore earum fugiat corrupti veniam iste ratione at perferendis. Optio explicabo at temporibus sint. Sint eum quas dolorem cupiditate pariatur, laudantium animi!</p>
+            <SearchForm />
+          </div>
+        </header>
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Header from '../../components/Header/Header';
+import { Outlet } from 'react-router-dom';
 
 export const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <main>
+      <Header />
+      <Outlet />
+    </main>
   )
 }
 
