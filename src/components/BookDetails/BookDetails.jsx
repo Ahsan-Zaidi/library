46aa606ea250670c007ca9be35115c62d792f3e0
Zaidi <zaidiahsan.r@gmail.com>
2023-01-10 18:08:10 -0600

    bookdetails page

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,9 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
-import Loading from '../Loader/Loader';
-import coverImg from '../../images/cover_not_found.jpg';
-import './BookDetails.css';
-import { FaArrowLeft } from 'react-icons/fa';
+import Loading from "../Loader/Loader";
+import coverImg from "../../images/cover_not_found.jpg";
+import "./BookDetails.css";
+import {FaArrowLeft} from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
 
 const URL = "https://openlibrary.org/works/";
@@ -16,20 +16,20 @@ const BookDetails = () => {
 
   useEffect(() => {
     setLoading(true);
-    async function getBookDetails() {
-      try {
+    async function getBookDetails(){
+      try{
         const response = await fetch(`${URL}${id}.json`);
-        const data = await response.json;
+        const data = await response.json();
         console.log(data);
 
-        if(data) {
+        if(data){
           const {description, title, covers, subject_places, subject_times, subjects} = data;
           const newBook = {
-            description: description ? description.value : "No Description Found",
+            description: description ? description.value : "No description found",
             title: title,
             cover_img: covers ? `https://covers.openlibrary.org/b/id/${covers[0]}-L.jpg` : coverImg,
             subject_places: subject_places ? subject_places.join(", ") : "No subject places found",
-            subject_times: subject_times ? subject_times.join(", ") : "No subject times found",
+            subject_times : subject_times ? subject_times.join(", ") : "No subject times found",
             subjects: subjects ? subjects.join(", ") : "No subjects found"
           };
           setBook(newBook);
@@ -37,7 +37,7 @@ const BookDetails = () => {
           setBook(null);
         }
         setLoading(false);
-      } catch(error) {
+      } catch(error){
         console.log(error);
         setLoading(false);
       }
@@ -51,20 +51,20 @@ const BookDetails = () => {
     <section className='book-details'>
       <div className='container'>
         <button type='button' className='flex flex-c back-btn' onClick={() => navigate("/book")}>
-          <FaArrowLeft size={22} />
+          <FaArrowLeft size = {22} />
           <span className='fs-18 fw-6'>Go Back</span>
         </button>
 
         <div className='book-details-content grid'>
           <div className='book-details-img'>
-            <img src={book?.cover_img} alt='cover img' />
+            <img src = {book?.cover_img} alt = "cover img" />
           </div>
           <div className='book-details-info'>
             <div className='book-details-item title'>
               <span className='fw-6 fs-24'>{book?.title}</span>
             </div>
             <div className='book-details-item description'>
-              <span className='fw-6 fs-24'>{book?.description}</span>
+              <span>{book?.description}</span>
             </div>
             <div className='book-details-item'>
               <span className='fw-6'>Subject Places: </span>
